bc2429fe9e75fea9cc627e1e87ca98bfb351f9f5
lukaa12 <l.wolanin@stud.elka.pw.edu.pl>
2019-11-08 13:01:04 +0100

    Dodano klasę Fence do przechowywania info o najdłuższym ogrodzeniu

--- venv/brutal.py
+++ venv/brutal.py
@@ -2,25 +2,50 @@ import numpy
 import loadTab
 import generator
 
+class Fence:
+    """Klasa zawierająca informacje o prostokątnym ogrodzeniu
+
+    x1,y1 - współrzędne pierwszego wierzchołka
+    x2,y2 - współrzędne drugiego wierzchołka
+    len - długość ogrodzenia"""
+
+    x1 = -1
+    y1 = -1
+    x2 = -1
+    y2 = -1
+    len = 0
+
+    def __init__(self, x1=-1, y1=-1, x2=-1, y2=-1):
+        self.x1, self.x2, self.y1, self.y2 = x1, x2, y1, y2
+        len = (y2-y1+x2-x1)*2
+
+    def show(self):
+        print(f"Max: {self.len} ({self.y1},{self.x1})-({self.y2},{self.x2})")
+
+
 def brutal(arr):
     """Funkcja znajdująca najdłuższe możliwe do ustawienia ogrodzenie metodą brute force"""
 
-    print(arr)
-    zz = 1
     h, w = arr.shape
- #   print(h,w)
-    max = [0, -1, -1, -1, -1]
+    max = Fence()
+    # max2 = [0, 0, 0, 0, 0]
     for y1 in range(h-1):
         for x1 in range(w-1):
             for y2 in range(y1+1,h):
                 for x2 in range(x1+1,w):
                     if sum(arr[y1,x1:x2+1])+sum(arr[y2,x1:x2+1])+sum(arr[y1+1:y2,x1])+sum(arr[y1+1:y2,x2])==0:
-                        if (y2-y1+x2-x1)*2 > max[0]:
-                            max = [(y2-y1+x2-x1)*2, y1, x1, y2, x2]
-    print(f"Max: {max[0]} ({max[1]},{max[2]})-({max[3]},{max[4]})")
+                        # if x1 == 0 and y1 == 1 and x2 == 2 and y2 == 2:
+                        #     print("HALO! ", (y2-y1+x2-x1)*2, max2[0])
+                        if (y2-y1+x2-x1)*2 > max.len:
+                        # if (y2 - y1 + x2 - x1) * 2 > max2[0]:
+                        #     max = Fence(x1,y1,x2,y2)
+                            max.x1, max.y1, max.x2, max.y2 = x1, y1, x2, y2
+                            max.len = (y2-y1+x2-x1)*2
+                            # max2 = [(y2-y1+x2-x1)*2, y1, x1, y2, x2]
+    # print(f"Max: {max2[0]} ({max2[1]},{max2[2]})-({max2[3]},{max2[4]})")
+    return max
 
-#mat = loadTab.load()
+maxi = brutal(loadTab.load())
+maxi.show()
 
-mat = generator.generate(7,10,0.2)
-brutal(mat)
 
--- venv/generator.py
+++ venv/generator.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 def generate(h, w, p):
+    """Funkcja generująca losowe dane do celów testowych.
+
+    Przyjmująca za parametry wysokość, szerokość i stopień 'zabagnienia' pola."""
+
     fill = np.random.rand(h*w) < p
-    # print(fill)
     matrix = np.zeros((h,w), int)
     for i in range(h*w):
         if fill[i]:
             matrix[i//w,i%w] = 1
 
     return matrix
-
-# print(generate(4,7,0.5))
