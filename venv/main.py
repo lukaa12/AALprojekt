1ed53ecf26b918af35311fe690f9d651cc3fc5a0
lukaa12 <l.wolanin@stud.elka.pw.edu.pl>
2019-11-18 20:31:26 +0100

    upgrade of main v2

--- venv/main.py
+++ venv/main.py
@@ -5,20 +5,23 @@ import brutal
 
 argLen = sys.argv.__len__()
 
-if argLen not in [2, 4, 6]:
-    print("Usage: -m1 or -m2 widthxheight density")
-    exit(-1)
-
 if argLen == 2 and sys.argv[1] == '-m1':
     matrix = loadTab.load()
     result = brutal.brutal(matrix)
     result.show()
 
 elif argLen == 4 and sys.argv[1] == '-m2':
-    #TODO
     wymiary = sys.argv[2].split('x')
     szer, wys = int(wymiary[0]), int(wymiary[1])
     matrix = generator.generate(wys,szer,float(sys.argv[3]))
     print(matrix)
     result = brutal.brutal(matrix)
     result.show()
+
+elif argLen == 6 and sys.argv[1] == '-m3':
+    null
+    #TODO
+
+else:
+    print("Usage: -m1 or -m2 widthxheight density")
+    exit(-1)
