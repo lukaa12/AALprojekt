Algorithm needs to be fixed, because i found that it gives wrong results

venv/function.py
@@ -1,7 +1,9 @@
-import brutal
 import generator
 import loadTab
 import numpy as np
+from fence import Fence
+from time import gmtime, strftime
+import brutal
 
 def func(arr):
     """Funkcja do znajdowania ogrodzenia, lepsza od metody brutalnej
@@ -18,8 +20,9 @@ def func(arr):
 
     left = np.zeros(arr.shape, int)
     up = np.zeros(arr.shape, int)
+    print(type(left))
     h, w = arr.shape
-    maxFence = brutal.Fence()
+    maxFence = Fence()
     for y in range(h):
         for x in range(w):
             if arr[y,x] == 0:
@@ -28,34 +31,35 @@ def func(arr):
                     up[y,x] += up[y-1,x]
                 if x > 0:
                     left[y, x] += left[y, x-1]
+            else:
+                left[y, x], up[y, x] = 0, 0
 
-    for y in range(h - 1, 0, -1):
-        for x in range(w - 1, 0, -1):
-            ph, pw = up[y,x], left[y,x]#potential height & width
-            if ph <= 1 or pw <= 1:
+    for y in range(h-1,0,-1):
+        for x in range(w-1,0,-1):
+            if left[y, x] <= 1 or up[y, x] <= 1:
                 continue
-            if (ph-1+pw-1)*2 <= maxFence.getLen():
+            if (left[y, x] + up[y, x] -2) * 2 <= maxFence.getLen():
                 continue
-            lptr = x -left[y,x] + 1
-            uptr = y - up[y,x] + 1
-            while lptr < x:
-                rptr = uptr
-                while rptr < y:
-                    height = x -rptr+1
-                    width = y -lptr+1
-                    if up[y,lptr] >= height and left[rptr,x] >= width:
-                        if (width-1+height-1) * 2 > maxFence.getLen():
-                            maxFence.y1, maxFence.x1, maxFence.y2, maxFence.x2 = rptr, lptr, y, x
+            for lptr in range(x - left[y, x] +1, x, 1):
+                for uptr in range(y - up[x, y] +1, y, 1):
+                    if (x - lptr + y - uptr) * 2 <= maxFence.getLen():
                         break
-                    rptr = rptr + 1
-                lptr = lptr +1
+                    if up[lptr, x] > y - uptr and left[y, uptr] > x - lptr: #???
+                        maxFence.reset(y, x, uptr, lptr)
     return maxFence
 
 if __name__ == "__main__":
-    # arr = generator.generate(5,5,0.0)
+    arr = generator.generate(5,5,0.2)
+    np.save("array" + strftime("%m%d%H%M%S", gmtime()), arr)
     # print(fenceSize(0,0,2,2))
-    arr = loadTab.load()
-    # print(arr)
+    # arr = loadTab.load()
+    print(arr)
     # print('-------------------')
     fencePredicted = func(arr)
     fencePredicted.show()
+    fencePredicted = brutal.brutal(arr)
+    fencePredicted.show()
+    for i in range(10):
+        if i % 2 == 0:
+            continue
+        print(i)

venv/loadTab.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 
 def load():
     """Funkcja do ładownaia danych ze standardowego wejścia o 'działce'"""
@@ -6,7 +6,7 @@ def load():
     wymiary = input("Wprowadź wymiary tablicy w formacie 'szer'x'wys': ")
     wymiary = wymiary.split('x')
     szer, wys = int(wymiary[0]), int(wymiary[1])
-    matrix = numpy.zeros((wys,szer), int)
+    matrix = np.zeros((wys,szer), int)
     print("Wprowadź dane wiersz po wierszu, gdzie _ wolne pole X to bagno:")
     for i in range(wys):
         str = input('')